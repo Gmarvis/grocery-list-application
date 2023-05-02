e83a06fb972908400403d150ffc611358a072650
gmarvis <sgmarvis@gmail.com>
2023-05-02 17:20:41 +0100

    added edit function

--- main.js
+++ main.js
@@ -1,53 +1,70 @@
-let form = document.querySelector('#myList')
-let itermComtainer = document.getElementById('iterm-comtainer')
+let form = document.querySelector("#myList");
+let itermComtainer = document.getElementById("iterm-comtainer");
 
+let taskHOlder = {
+  task: "",
+  domEl: {},
+};
 
+const editTask = (updates) => {
+  taskHOlder.domEl.childNodes[0].textContent = updates;
+  taskHOlder = {};
+};
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formInput = document.getElementById("listIterm");
 
-    // prevent empty list submition
-    let listIterm = document.getElementById('listIterm').value
+  // prevent empty list submition
+  let listIterm = formInput.value;
 
-    if (listIterm ===""){
-    return alert("Plaese enter iterm name...")
-    }
+  if (taskHOlder.task) {
+    editTask(formInput.value);
 
+    formInput.value = "";
+    return;
+  }
 
-    // create a an html element li for new list iterms
+  formInput.value = "";
 
-    let newIterms = document.createElement('li')
-    newIterms.innerText=listIterm
-    itermComtainer.appendChild(newIterms)
-    // create a span to carry our buttons
+  if (listIterm === "") {
+    return alert("Plaese enter iterm name...");
+  }
 
-    let actionBnt = document.createElement('span')
-    newIterms.appendChild(actionBnt)
+  // create a an html element li for new list iterms
 
-// edit button
-        let editBtn = document.createElement('button')
-        editBtn.innerHTML=`<i class="fa-sharp fa-solid fa-pen-to-square"></i>`
-        actionBnt.appendChild(editBtn)
+  let newIterms = document.createElement("li");
+  newIterms.innerText = listIterm;
+  itermComtainer.appendChild(newIterms);
 
-    // iterms delete btn
-    let dlBtn = document.createElement('button')
-    dlBtn.innerHTML=`<i class="fa-solid fa-trash"></i>`
-    actionBnt.appendChild(dlBtn)
+  // create a span to carry our buttons
 
-    // adding function to the delete button 
-    dlBtn.addEventListener('click', ()=>{
-        newIterms.remove()
-    })
+  let actionBnt = document.createElement("span");
+  newIterms.appendChild(actionBnt);
 
-    // add the edit iterms function
+  // edit button
+  let editBtn = document.createElement("button");
+  editBtn.innerHTML = `<i class="fa-sharp fa-solid fa-pen-to-square"></i>`;
+  actionBnt.appendChild(editBtn);
 
-    editBtn.addEventListener('click',()=>{
-        newIterms.prefix(2)
-    })
+  // iterms delete btn
+  let dlBtn = document.createElement("button");
+  dlBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+  actionBnt.appendChild(dlBtn);
 
+  // adding function to the delete button
+  dlBtn.addEventListener("click", () => {
+    newIterms.remove();
+  });
 
+  // add the edit iterms function
 
-    // console.log(newIterms).value
-    console.log(listIterm)
+  editBtn.addEventListener("click", () => {
+    formInput.value = newIterms.innerText;
+    taskHOlder.task = newIterms.innerText;
+    taskHOlder.domEl = newIterms;
+  });
 
-})
+  // console.log(newIterms).value
+  console.log(listIterm);
+});
